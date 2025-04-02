d06e828b8668275bce5fa5a040f6f3f125e4873e
nb510 <nikita.burak.dev@gmail.com>
2025-04-02 23:32:41 +0300

    add logging

--- src/main/java/guru/springframework/spring6restmvc/service/BeerServiceImpl.java
+++ src/main/java/guru/springframework/spring6restmvc/service/BeerServiceImpl.java
@@ -2,6 +2,7 @@ package guru.springframework.spring6restmvc.service;
 
 import guru.springframework.spring6restmvc.model.Beer;
 import guru.springframework.spring6restmvc.model.BeerStyle;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -9,10 +10,12 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Service
+@Slf4j
 public class BeerServiceImpl implements BeerService {
 
     @Override
     public Beer getBeerById(UUID id) {
+        log.debug("Trying to get a beer");
         return Beer.builder()
                 .id(id)
                 .version(1)
