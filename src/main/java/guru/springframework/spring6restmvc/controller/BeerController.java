2cbf6c9f355cdb64bd39a64739da975ecdb222b6
nb510 <nikita.burak.dev@gmail.com>
2025-04-03 22:06:34 +0300

    refactor URI in BeerController

--- src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,16 +15,16 @@ import java.util.UUID;
 @RestController
 @AllArgsConstructor
 @Slf4j
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/beer")
 public class BeerController {
     private final BeerService beerService;
 
-    @RequestMapping("/beer")
+    @RequestMapping(method = RequestMethod.GET)
     public List<Beer> listBeers() {
         return beerService.listBeers();
     }
 
-    @RequestMapping("/beer/{beerId}")
+    @RequestMapping(value = "/{beerId}", method = RequestMethod.GET)
     public Beer getBeerById(@PathVariable("beerId") UUID id){
         log.debug("Get Beer by Id - in controller");
         return beerService.getBeerById(id);
