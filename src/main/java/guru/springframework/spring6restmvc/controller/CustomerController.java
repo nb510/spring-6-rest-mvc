cc13527e1be6c9f7c9ec8a55c357b6bf9981362f
nb510 <nikita.burak.dev@gmail.com>
2025-04-03 22:05:25 +0300

    add Customer

---
+++ src/main/java/guru/springframework/spring6restmvc/controller/CustomerController.java
@@ -0,0 +1,30 @@
+package guru.springframework.spring6restmvc.controller;
+
+import guru.springframework.spring6restmvc.model.Customer;
+import guru.springframework.spring6restmvc.service.CustomerService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/v1/customer")
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Customer> listCustomers() {
+        return customerService.listCustomers();
+    }
+
+    @RequestMapping(value = "/{customerId}", method = RequestMethod.GET)
+    public Customer getCustomerById(@PathVariable("customerId") UUID id) {
+        return customerService.getCustomerById(id);
+    }
+}
