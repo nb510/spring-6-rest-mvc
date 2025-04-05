188b699f4552928629e5dc042fbb6c718fb25816
nb510 <nikita.burak.dev@gmail.com>
2025-04-05 20:09:31 +0300

    add Mockito mock example

--- src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerTest.java
@@ -1,5 +1,6 @@
 package guru.springframework.spring6restmvc.controller;
 
+import guru.springframework.spring6restmvc.model.Beer;
 import guru.springframework.spring6restmvc.service.BeerService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +11,10 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.UUID;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(BeerController.class)
@@ -23,10 +27,13 @@ class BeerControllerTest {
     BeerService beerService;
 
     @Test
-    void testLogging() throws Exception {
+    void testGetBeerById() throws Exception {
+        when(beerService.getBeerById(any(UUID.class))).thenReturn(Beer.builder().id(UUID.randomUUID()).build());
+
         mockMvc.perform(get("/api/v1/beer/%s".formatted(UUID.randomUUID()))
                         .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
     }
 
 }
