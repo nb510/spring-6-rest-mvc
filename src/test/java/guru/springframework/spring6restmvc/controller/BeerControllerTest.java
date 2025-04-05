1e16ec24df3f4a3a992161da16a515aeeec9fa99
nb510 <nikita.burak.dev@gmail.com>
2025-04-05 20:32:56 +0300

    add list beer test

--- src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerTest.java
@@ -9,9 +9,11 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
 import java.util.UUID;
 
 import static org.hamcrest.core.Is.is;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -24,6 +26,18 @@ class BeerControllerTest {
 
     @MockBean
     BeerService beerService;
+
+    @Test
+    void testListBeer() throws Exception {
+        Beer beer1 = Beer.builder().id(UUID.randomUUID()).build();
+        Beer beer2 = Beer.builder().id(UUID.randomUUID()).build();
+        given(beerService.listBeers()).willReturn(List.of(beer1, beer2));
+
+        mockMvc.perform(get("/api/v1/beer").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.length()",  is(2)));
+    }
 
     @Test
     void testGetBeerById() throws Exception {
