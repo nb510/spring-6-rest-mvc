c91cf6f5cabc025df040f60fbc581da51fff7e8e
nb510 <nikita.burak.dev@gmail.com>
2025-04-05 20:41:54 +0300

    add tests for CustomerController

---
+++ src/test/java/guru/springframework/spring6restmvc/controller/CustomerControllerTest.java
@@ -0,0 +1,54 @@
+package guru.springframework.spring6restmvc.controller;
+
+import guru.springframework.spring6restmvc.model.Customer;
+import guru.springframework.spring6restmvc.service.CustomerService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.hamcrest.Matchers.is;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(CustomerController.class)
+class CustomerControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    CustomerService customerService;
+
+    @Test
+    public void testListCustomers() throws Exception {
+        Customer customer1 = Customer.builder().id(UUID.randomUUID()).build();
+        Customer customer2 = Customer.builder().id(UUID.randomUUID()).build();
+        given(customerService.listCustomers()).willReturn(List.of(customer1, customer2));
+
+        mockMvc.perform(get("/api/v1/customer").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()", is(2)));
+    }
+
+    @Test
+    public void testGetCustomerById() throws Exception {
+        Customer customer = Customer.builder()
+                .id(UUID.randomUUID())
+                .customerName("Classic")
+                .build();
+        given(customerService.getCustomerById(customer.getId())).willReturn(customer);
+
+        mockMvc.perform(get("/api/v1/customer/%s".formatted(customer.getId())).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(customer.getId().toString())))
+                .andExpect(jsonPath("$.customerName", is(customer.getCustomerName())));
+    }
+}
