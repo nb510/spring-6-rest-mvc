65021aaf3166c1cc891a49827df3aaf3fa3cd322
nb510 <nikita.burak.dev@gmail.com>
2025-04-16 21:09:08 +0300

    add jpa services

--- src/main/java/guru/springframework/spring6restmvc/repository/CustomerRepository.java
+++ src/main/java/guru/springframework/spring6restmvc/repository/CustomerRepository.java
@@ -4,6 +4,8 @@ import guru.springframework.spring6restmvc.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
+public interface CustomerRepository extends JpaRepository<Customer, UUID> {
 }
---
+++ src/main/java/guru/springframework/spring6restmvc/service/BeerServiceJpa.java
@@ -0,0 +1,53 @@
+package guru.springframework.spring6restmvc.service;
+
+import guru.springframework.spring6restmvc.mappers.BeerMapper;
+import guru.springframework.spring6restmvc.model.BeerDto;
+import guru.springframework.spring6restmvc.repository.BeerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@Primary
+@RequiredArgsConstructor
+public class BeerServiceJpa implements BeerService {
+    private final BeerRepository beerRepository;
+    private final BeerMapper beerMapper;
+
+    @Override
+    public List<BeerDto> listBeers() {
+        return beerRepository.findAll()
+                .stream()
+                .map(beerMapper::toBeerDto)
+                .toList();
+    }
+
+    @Override
+    public Optional<BeerDto> getBeerById(UUID id) {
+        return beerRepository.findById(id).map(beerMapper::toBeerDto);
+    }
+
+    @Override
+    public BeerDto createBeer(BeerDto beer) {
+        return null;
+    }
+
+    @Override
+    public void updateBeerById(UUID id, BeerDto beer) {
+
+    }
+
+    @Override
+    public void deleteBeerById(UUID id) {
+
+    }
+
+    @Override
+    public void patchBeer(UUID id, BeerDto beer) {
+
+    }
+}
