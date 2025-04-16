5d2efe07a229e05d92198829e075f3ffeaba24d4
nb510 <nikita.burak.dev@gmail.com>
2025-04-16 19:56:55 +0300

    add tests for repositories

---
+++ src/test/java/guru/springframework/spring6restmvc/repository/CustomerRepositoryTest.java
@@ -0,0 +1,20 @@
+package guru.springframework.spring6restmvc.repository;
+
+import guru.springframework.spring6restmvc.entities.Customer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+class CustomerRepositoryTest {
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Test
+    void testSavingCustomer() {
+        Customer result = customerRepository.save(Customer.builder().build());
+        assertThat(result.getId()).isNotNull();
+    }
+}
