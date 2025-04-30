61d0589f40c6b51afa025df16e850d50001da189
nb510 <nikita.burak.dev@gmail.com>
2025-04-30 13:25:40 +0300

    add more informative message after JPA constraint violation

--- src/main/java/guru/springframework/spring6restmvc/controller/ValidationExceptionHandler.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/ValidationExceptionHandler.java
@@ -1,7 +1,9 @@
 package guru.springframework.spring6restmvc.controller;
 
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.TransactionSystemException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,6 +15,24 @@ import java.util.Map;
 
 @ControllerAdvice
 public class ValidationExceptionHandler {
+
+    @ExceptionHandler(TransactionSystemException.class)
+    public ResponseEntity<Map<String, List<String>>> handleJpaValidationException(TransactionSystemException exception) {
+        if (exception.getCause().getCause() instanceof ConstraintViolationException jpaException) {
+            Map<String, List<String>> errors = new HashMap<>(jpaException.getConstraintViolations().size());
+
+            jpaException.getConstraintViolations().forEach(c -> {
+                String fieldName = c.getPropertyPath().toString();
+                String errorMessage = c.getMessage();
+
+                errors.computeIfAbsent(fieldName, k -> new ArrayList<>()).add(errorMessage);
+            });
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+        }
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, List<String>>> handleValidationExceptions(MethodArgumentNotValidException exception) {
