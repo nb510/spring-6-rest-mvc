7294722d695bad536af31d4bd3869e19adb6431d
nb510 <nikita.burak.dev@gmail.com>
2025-04-30 08:46:50 +0300

    add string size constraint

--- src/test/java/guru/springframework/spring6restmvc/repository/BeerRepositoryTest.java
+++ src/test/java/guru/springframework/spring6restmvc/repository/BeerRepositoryTest.java
@@ -2,6 +2,7 @@ package guru.springframework.spring6restmvc.repository;
 
 import guru.springframework.spring6restmvc.entities.Beer;
 import guru.springframework.spring6restmvc.model.BeerStyle;
+import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -9,12 +10,27 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DataJpaTest
 class BeerRepositoryTest {
 
     @Autowired
     BeerRepository beerRepository;
+
+    @Test
+    void testLongString() {
+        assertThrows(ConstraintViolationException.class, () -> {
+            beerRepository.save(Beer.builder()
+                    .beerName("Crank toooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo long")
+                    .beerStyle(BeerStyle.PALE_ALE)
+                    .upc("12356222")
+                    .price(new BigDecimal("11.99"))
+                    .quantityOnHand(392)
+                    .build());
+            beerRepository.flush();
+        });
+    }
 
     @Test
     void testSavingBeer() {
