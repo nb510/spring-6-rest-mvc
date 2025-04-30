959e38d243c5f1536bc3e46b4fd7a4134622003c
nb510 <nikita.burak.dev@gmail.com>
2025-04-30 07:06:32 +0300

    add beer entity validation

--- src/test/java/guru/springframework/spring6restmvc/repository/BeerRepositoryTest.java
+++ src/test/java/guru/springframework/spring6restmvc/repository/BeerRepositoryTest.java
@@ -1,9 +1,12 @@
 package guru.springframework.spring6restmvc.repository;
 
 import guru.springframework.spring6restmvc.entities.Beer;
+import guru.springframework.spring6restmvc.model.BeerStyle;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,7 +18,14 @@ class BeerRepositoryTest {
 
     @Test
     void testSavingBeer() {
-        Beer result = beerRepository.save(Beer.builder().build());
+        Beer result = beerRepository.save(Beer.builder()
+                .beerName("Crank")
+                .beerStyle(BeerStyle.PALE_ALE)
+                .upc("12356222")
+                .price(new BigDecimal("11.99"))
+                .quantityOnHand(392)
+                .build());
+        beerRepository.flush();
         assertThat(result.getId()).isNotNull();
     }
 }
