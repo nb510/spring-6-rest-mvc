c13219fcb90709a7a577b257ea68cea8b99f3a9e
nb510 <nikita.burak.dev@gmail.com>
2025-05-05 15:42:22 +0300

    use ServiceConnection to avoid setting properties for mysql container manually

--- src/test/java/guru/springframework/spring6restmvc/repository/MySqlTest.java
+++ src/test/java/guru/springframework/spring6restmvc/repository/MySqlTest.java
@@ -1,13 +1,11 @@
-package guru.springframework.spring6restmvc.repositories;
+package guru.springframework.spring6restmvc.repository;
 
 import guru.springframework.spring6restmvc.entities.Beer;
-import guru.springframework.spring6restmvc.repository.BeerRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -25,14 +23,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class MySqlTest {
 
     @Container
+    @ServiceConnection
     static MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:8");
-
-    @DynamicPropertySource
-    static void mySqlProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.username", mySQLContainer::getUsername);
-        registry.add("spring.datasource.password", mySQLContainer::getPassword);
-        registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
-    }
 
     @Autowired
     BeerRepository beerRepository;
