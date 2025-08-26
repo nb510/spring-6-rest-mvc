1eb905502eb25f3f3744b6898d75a1083acea10a
nb510 <nikita.burak.dev@gmail.com>
2025-08-26 17:35:31 +0300

    add REST Assured test

---
+++ src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerRestAssuredTest.java
@@ -0,0 +1,52 @@
+package guru.springframework.spring6restmvc.controller;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.test.context.ActiveProfiles;
+
+import static io.restassured.RestAssured.given;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles({"test"})
+@Import(BeerControllerRestAssuredTest.TestSecurityConfig.class)
+@ComponentScan(basePackages = "guru.springframework.spring6restmvc")
+public class BeerControllerRestAssuredTest {
+
+    @Configuration
+    public static class TestSecurityConfig {
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+            return http.authorizeHttpRequests(customizer -> customizer.anyRequest().permitAll()).build();
+        }
+    }
+
+    @LocalServerPort
+    Integer localPort;
+
+    @BeforeEach
+    void setup() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = localPort;
+    }
+
+    @Test
+    void testListBeer() {
+        given().contentType(ContentType.JSON)
+                .when()
+                .get("/api/v1/beer")
+                .then()
+                .assertThat().statusCode(200);
+
+    }
+
+}
