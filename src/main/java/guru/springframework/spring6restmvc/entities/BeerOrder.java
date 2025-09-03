a4e83400558c80f8bd55e92cf21ba296cf52c9dd
nb510 <nikita.burak.dev@gmail.com>
2025-09-03 15:07:01 +0300

    add Lazy loading for order shipment

--- src/main/java/guru/springframework/spring6restmvc/entities/BeerOrder.java
+++ src/main/java/guru/springframework/spring6restmvc/entities/BeerOrder.java
@@ -60,7 +60,7 @@ public class BeerOrder {
 
     private String customerRef;
 
-    @OneToOne(cascade = CascadeType.PERSIST, orphanRemoval = true)
+    @OneToOne(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
     @JoinColumn(name = "beer_order_shipment_id", unique = true)
     private BeerOrderShipment beerOrderShipment;
 
