d9bc435cdb37ef557ca1162823bddc75b223d2ed
nb510 <nikita.burak.dev@gmail.com>
2025-09-04 20:02:01 +0300

    fix order deletion

--- src/main/java/guru/springframework/spring6restmvc/entities/BeerOrder.java
+++ src/main/java/guru/springframework/spring6restmvc/entities/BeerOrder.java
@@ -60,7 +60,7 @@ public class BeerOrder {
 
     private String customerRef;
 
-    @OneToOne(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
+    @OneToOne(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY, orphanRemoval = true)
     @JoinColumn(name = "beer_order_shipment_id", unique = true)
     private BeerOrderShipment beerOrderShipment;
 
